c84d57d611f216597f80930eb184ca942389efea
AjithAnalytics <147236939+AjithAnalytics@users.noreply.github.com>
2024-01-25 15:18:31 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
-# Industrial_Copper_Modeling
+
+# Industrial-Copper-Modeling-Project
+
+## project overview
+This project aims to develop two machine learning models for the copper industry to address the challenges of predicting selling price and lead classification. Manual predictions can be time-consuming and may not result in optimal pricing decisions or accurately capture leads. The models will utilize advanced techniques such as data normalization, outlier detection and handling, handling data in the wrong format, identifying the distribution of features, and leveraging tree-based models, specifically the decision tree algorithm, to predict the selling price and leads accurately.
+ ```
+## Key Technologies and Skills
+```bash
+* Python
+* Pandas
+* Scikit-learn
+* Streamlit
+```
+## solutions
+```http
+The solution includes the following steps:
+
+Exploring skewness and outliers in the dataset.
+
+Transforming the data into a suitable format and performing any necessary cleaning and pre-processing steps.
+
+Developing a machine learning regression model which predicts the continuous variable 'Selling_Price' using the decision tree regressor.
+
+Developing a machine learning classification model which predicts the Status: WON or LOST using the decision tree classifier.
+
+Creating a Streamlit page where you can insert each column value and get the Selling_Price predicted value or Status (Won/Lost).
+```
+
+## Installation
+Install need project packages
+* Pandas: Essential for data handling and manipulation.
+```bash
+                            import pandas as pd
+```
+* NumPy: Essential for numerical operations and data handling.
+```bash
+                            import numpy as np
+```
+* Scikit-learn: Scikit-learn is an open-source machine learning library for Python that provides simple and efficient tools for data analysis and modeling
+```bash
+Streamlit:
+Streamlit is an open-source Python library that simplifies the process of creating web applications for data science and machine learning. It allows users to turn data scripts into shareable web apps with minimal effort
+```
+```bash
+                            streamlit run app.py
+```
+# Regression model details
+
+```bash
+The copper industry deals with less complex data related to sales and pricing. However, this data may suffer from issues such as skewness and noisy data, which can affect the accuracy of manual predictions. Dealing with these challenges manually can be time-consuming and may not result in optimal pricing decisions. A machine learning regression model can address these issues by utilizing advanced techniques such as data normalization, outlier detection and handling, handling data in wrong format, identifying the distribution of features, and leveraging tree-based models, specifically the decision tree algorithm.
+```
+## Classification model details
+
+Another area where the copper industry faces challenges is in capturing the leads. A lead classification model is a system for evaluating and classifying leads based on how likely they are to become a customer. You can use the STATUS variable with WON being considered as Success and LOST being considered as Failure and remove data points other than WON, LOST STATUS values.
+## Contact
+📧 Email: farmerajith@gmail.com
+
+🌐 LinkedIn: www.linkedin.com/in/ajith-kumar-r-344314296
+
+For any further questions or inquiries, feel free to reach out. We are happy to assist you with any queries.
---
+++ app.py
@@ -0,0 +1,111 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+import pickle
+import numpy as np
+from datetime import date
+
+def predict_price(predict_data):
+    with open('C:\pythan\DATA SCEINCE\Copper Modeling\Regression_model_Prdict_Price.pkl', 'rb' ) as file:
+        model = pickle.load(file)
+        data = np.exp(model.predict(predict_data))
+
+        return data
+
+def predict_status(predict_data):
+    with open('C:\pythan\DATA SCEINCE\Copper Modeling\classification_model_Predict_Status.pkl', 'rb' ) as file:
+        model = pickle.load(file)
+        data = model.predict(predict_data)
+        if data == 1.0:
+          return 'Won'
+        else:
+          return 'Lost'
+
+item_type_value = {'W': 5.0, 'S': 3.0, 'Others': 1.0, 'PL': 2.0, 'WI': 6.0, 'IPL': 0.0, 'SLAWR': 4.0}
+
+status_values = {'Won': 7.0, 'Draft': 0.0, 'To be approved': 6.0, 
+                'Lost': 1.0, 'Not lost for AM': 2.0, 'Wonderful': 8.0,
+                'Revised': 5.0, 'Offered': 4.0, 'Offerable': 3.0}
+
+classification_status = {'Won': 1.0, 'Lost': 0.0}
+
+application_code = [10., 41., 28., 59., 15.,  4., 38., 56., 42., 26., 27., 19., 20.,
+                66., 29., 22., 40., 25., 67., 79.,  3., 99.,  2.,  5., 39., 69.,
+                70., 65., 58., 68.]
+
+country_code = [ 28.,  25.,  30.,  32.,  38.,  78.,  27.,  77., 113.,  79.,  26.,
+                 39.,  40.,  84.,  80., 107.,  89.]
+
+# SETTING PAGE CONFIGURATIONS
+st.set_page_config(page_title= "Industrial Copper Modeling Appication",
+                   layout= "wide",
+                   initial_sidebar_state= "expanded",
+                   menu_items={'About': """# This Copper modeling app is created by R.AJITH KUMAR!"""})
+st.markdown("<h1 style='text-align: center; color: white;'> Industrial Copper Modeling Appication</h1>", unsafe_allow_html=True)
+
+# SETTING-UP BACKGROUND IMAGE
+st.markdown("""
+    <style>
+        .stApp {
+            background: linear-gradient(to right,#cfd8dc, #90a4ae, #607d8b, #37474f, #263238);
+            background-size: cover;
+        }
+    </style>
+""", unsafe_allow_html=True)
+
+# CREATING OPTION MENU
+selected = option_menu(None, ["Home","PREDICT SELLING PRICE","PREDICT STATUS"], 
+                       icons=["house","dollar-sign","chart-line"],
+                       default_index=0,
+                       orientation="horizontal",
+                       styles={"nav-link": {"font-size": "25px", "text-align": "centre", "margin": "0px", "--hover-color": "#90a4ae"},
+                               "icon": {"color": "red","font-size": "25px"},
+                               "container" : {"max-width": "6000px"},
+                               "nav-link-selected": {"background-color": " #37474f"}})
+
+if selected == "Home":
+    col1,col2 = st.columns(2, gap = 'small')
+    with col1:
+       st.markdown("## :green[**Project:**] Business Card Data Extracting with OCR ")
+
+if selected == "PREDICT SELLING PRICE":
+    col1,col2,col3 = st.columns([0.5,0.1,0.5])
+    with col1:
+        item_date = st.date_input("Select the **Item Date**", date(2020, 7,2), min_value= date(2020, 7,2), max_value= date(2021, 4,1))
+
+        quantity_tons = st.number_input('Enter the **Quantity Tons**', min_value = 1e-05, max_value= 1000000000.0, value = 5874.904 )
+
+        customer = st.number_input('Enter the **Customer**', min_value = 12458.0, max_value= 2147483647.0, value = 30512207.0 )
+
+        status = st.selectbox('Select The **Status**', status_values)
+
+        item_type =  st.selectbox('Select The **Item Type**', item_type_value)
+
+        application = st.selectbox('Select The **Application**', application_code)
+    with col3:
+        
+        country = st.selectbox('Select the **Country**', country_code)
+
+        thickness = st.number_input('Enter the **Thickness**', min_value = 0.18, max_value= 2500.0, value = 2.56482 )
+
+        width = st.number_input('Enter the **Width**', min_value = 700.0, max_value= 1980.0, value = 1297.0455 )
+
+        product_ref = st.number_input('Enter the **Product Reference**', min_value = 611728.0, max_value= 1722207590.0, value = 473967910.72 )
+
+        delivery_date= st.date_input("Select the **Delivery Date**", date(2019, 4,1), min_value= date(2019, 4,1), max_value= date(2022, 1,1))
+
+    st.markdown('Click below button to predict the **Selling Price**')
+    prediction = st.button('**Predict**')
+
+    day_difference = item_date - delivery_date
+
+    quantity_tons_log = np.log1p(quantity_tons)
+    thickness_log = np.log1p(thickness)
+    
+    predict_data =[[quantity_tons, customer, country,  status_values[status], item_type_value[item_type], application, thickness, width, product_ref, 
+            quantity_tons_log, thickness_log ,day_difference.days, item_date.day, item_date.month, item_date.year,
+            delivery_date.day, delivery_date.month, delivery_date.year]]
+    
+    if prediction:
+        st.markdown(f"### :blue[Selling Price :] :green[$ {int(round(predict_price(predict_data)))}]")
+
+        # st.markdown(f"### :bule[Selling Price :] :green[$ {round(predict_price( predict_data))}]")
